e51e1f4affcc61913a9349061ea838b5f4022cf3
eakirkpatrick <eakirkpatrick@github.com>
2020-05-18 05:12:50 +0000

    working model

--- LSTMModel.py
+++ LSTMModel.py
@@ -5,6 +5,7 @@ from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.layers import Dense
 from dataCleaner import DataHandler
+import matplotlib.pyplot as plt
 
 
 class LSTMModel:
@@ -19,9 +20,7 @@ class LSTMModel:
 
 	def buildModel(self, number_of_layers):
 		model = Sequential()
-		model.add(LSTM(units=self.output_space,
-			return_sequences=True, input_shape=self.input_shape
-			)
+		model.add(LSTM(units=self.output_space,return_sequences=True, input_shape=self.input_shape))
 
 		model.add(Dropout(self.dropout_percent))
 
@@ -41,16 +40,26 @@ class LSTMModel:
 
 		return model
 
-	def train(x_train, y_train, model):
+	def train(self, x_train, y_train, model):
 		trained_model = model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size)
-		return trained_model
+		return model
 
-mydata = DataHandler(10)
+myData = DataHandler(10)
+model = LSTMModel('thisData.csv', 10, 60, 0.2, 1, (10, len(myData.train_columns)))
+df = model.dataset_train
 test, train = myData.splitData(df)
 test, train = myData.normalize(test, train)
 x_train, y_train = myData.createInputSequence(train, 4)
 x_test, y_test = myData.createInputSequence(test, 4)
 
-model = LSTMModel('thisData.csv', 100, 60, 0.2, 1, len(mydata.train_columns))
 myModel = model.buildModel(3)
 trained_model = model.train(x_train, y_train, myModel)
+
+prediction = trained_model.predict(x_test)
+prediction = myData.sc.inverse_transform(prediction)
+real_price = myData.sc.inverse_transform(y_test)
+
+plt.plot(real_price, color='red', label='Real Price')
+plt.plot(prediction, color='blue', label='Predicted Price')
+plt.legend()
+plt.show()
