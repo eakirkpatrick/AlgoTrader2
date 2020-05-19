2f7d8ad53d8162bba81e5e47aa95104e31c808c9
Everett Kirkpatrick <43871055+eakirkpatrick@users.noreply.github.com>
2020-05-18 22:17:48 -0600

    Add files via upload

---
+++ DRL.py
@@ -0,0 +1,62 @@
+import gym
+import numpy as np
+import random
+from keras.models import Sequential
+from keras.models import Dense, Dropout
+from keras.models import LSTM
+from keras.optimizers import Adam
+from LSTMModel import LSTMModel
+
+from collections import deque
+
+class DeepReinforcementLearn:
+    def __init__(self, env):
+        self.env = env
+        self.memory = deque(maxlen=2000)
+
+        self.gamma = 0.85
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.005
+        self.tau = .125
+
+        self.model = LSTMModel('thisData.csv', 5, )
+        self.target_model = LSTMModel()
+
+    def act(self, state):
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon_min, self.epsilon)
+
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+        return np.argmax(self.model.predict(state)[0])
+
+    def remember(self, state, action, reward, new_state, done):
+        self.memory.append([state, action, reward, new_state, done])
+
+    def replay(self):
+        batch_size = 32
+        if len(self.memory) < batch_size:
+            return
+
+        samples = random.sample(self.memory, batch_size)
+        for sample in samples:
+            state, action, reward, new_state, done = sample
+            target = self.target_model.predict(state)
+            if done:
+                target[0][action] = reward
+            else:
+                Q_future = max(self.target_model.predict(new_state)[0])
+                target[0][action] = reward + Q_future * self.gamma
+            self.model.fit(state, target, epochs=1, verbose=0)
+
+    def target_train(self):
+        weights = self.model.get_weights()
+        target_weights = self.target_model.get_weights()
+        for i in range(len(target_weights)):
+            target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
+        self.target_model.set_weights(target_weights)
+
+    def save_model(self, fn):
+        self.model.save(fn)
